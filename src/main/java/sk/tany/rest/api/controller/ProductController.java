a4d2ebe2b0827b9bd2b80b13d8f19fb2f148dcb6
Ľubomír Grňo <lgrno@trask.cz>
2025-12-31 23:28:35 +0100

    Add get product to controller

--- src/main/java/sk/tany/rest/api/controller/ProductController.java
+++ src/main/java/sk/tany/rest/api/controller/ProductController.java
@@ -3,8 +3,10 @@ package sk.tany.rest.api.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sk.tany.rest.api.dto.ProductDto;
@@ -21,6 +23,13 @@ public class ProductController {
     @GetMapping
     public Page<ProductDto> getProducts(Pageable pageable) {
         return productService.findAll(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable String id) {
+        return productService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
