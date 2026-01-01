e8e668109869e75e744ab2691e88aac1b71a83ee
Ľubomír Grňo <lgrno@trask.cz>
2026-01-01 18:44:18 +0100

    Categories endpoint

---
+++ src/main/java/sk/tany/rest/api/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package sk.tany.rest.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sk.tany.rest.api.dto.CategoryDto;
+import sk.tany.rest.api.service.CategoryService;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public Page<CategoryDto> getCategories(Pageable pageable) {
+        return categoryService.findAll(pageable);
+    }
+
+}
