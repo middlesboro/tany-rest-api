1b34ea1450cd18c23ab97320a40fb26b30a67417
Ľubomír Grňo <lgrno@trask.cz>
2026-01-30 11:30:32 +0100

    Add title

--- src/main/java/sk/tany/rest/api/domain/homepage/HomepageGrid.java
+++ src/main/java/sk/tany/rest/api/domain/homepage/HomepageGrid.java
@@ -2,12 +2,14 @@ package sk.tany.rest.api.domain.homepage;
 
 import lombok.Data;
 import org.dizitart.no2.objects.Id;
+
 import java.util.List;
 
 @Data
 public class HomepageGrid {
     @Id
     private String id;
+    private String title;
     private String brandId;
     private String categoryId;
     private List<String> productIds;
